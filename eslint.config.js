8f09baef7fdedf81807c1425d804c5eae6fd4ad5
thenatarizkie <natarizkie7@gmail.com>
2025-01-12 13:41:46 +0700

    chore: remove trailing newline in eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,3 @@ import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [{ languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended];
-
