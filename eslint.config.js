b8f8d3ad238b6d1487795d0c9fc86b9764cc1e8f
thenatarizkie <natarizkie7@gmail.com>
2025-01-12 13:40:23 +0700

    chore: add trailing newline to eslint config file for consistency

--- eslint.config.js
+++ eslint.config.js
@@ -3,3 +3,4 @@ import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [{ languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended];
+
