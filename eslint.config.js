4097de2656fd33a2e6d107b52a021230d0d890eb
thenatarizkie <natarizkie7@gmail.com>
2025-01-12 13:49:15 +0700

    chore: update eslint configuration by adding a trailing newline

--- eslint.config.js
+++ eslint.config.js
@@ -3,3 +3,6 @@ import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [{ languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended];
+
+
+
